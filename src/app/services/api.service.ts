938bb0228713909610daf2d76fbfd30e502736d7
Anthony Cruz <anthonyleierlw@gmail.com>
2021-01-25 22:23:31 -0300

    Integração de Histórico com API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,35 +6,23 @@ import {Injectable} from '@angular/core';
 })
 export class ApiService {
 	private API_URL = 'https://www.mercadobitcoin.net/api/';
-	
 	constructor(private http: HttpClient) {}
-
 
 	getCoin(moeda: string) {
 		let url = this.API_URL + moeda + '/ticker/';
 		return this.http.get(url);
 	}
-	getDolar(){
-        let urlDolar = 'https://economia.awesomeapi.com.br/all/USD-BRL';
-        return this.http.get(urlDolar);
-    }
-	/*funcao pegar data atual*/
-	getData(moeda: string) {
-		var valorMensal	;
-		var data = new Date();
-		var dia = data.getDate();
-		var mes = data.getMonth() + 1;
-		var ano = data.getFullYear();
-		let url = this.API_URL + moeda + '/day-summary/'+ ano + '/'+ mes + '/' + (dia-1);
-		return this.http.get(url);
-	/*	for(var i=1; i>dia;i++){
-			let url = this.API_URL + moeda + '/day-summary/'+ ano + '/'+ mes + '/' + i;
-			valorMensal[i]=  this.http.get(url);
-		}
-	*/
-	//	return this.http.get('https://www.mercadobitcoin.net/api/BTC/day-summary/2020/12/20/');
-		
+
+	getDolar() {
+		let urlDolar = 'https://economia.awesomeapi.com.br/all/USD-BRL';
+		return this.http.get(urlDolar);
 	}
 
-	
+	getMensal(moeda: string, dia: number) {
+		var data = new Date();
+		var mes = data.getMonth() + 1;
+		var ano = data.getFullYear();
+		let url = this.API_URL + moeda + '/day-summary/' + ano + '/' + mes + '/' + dia;
+		return this.http.get(url);
+	}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {ApiService} from '../services/api.service';
-import { GoogleChartInterface } from 'ng2-google-charts';
+import {GoogleChartInterface} from 'ng2-google-charts';
 
 @Component({
 	selector: 'app-tab2',
 	templateUrl: 'tab2.page.html',
 	styleUrls: ['tab2.page.scss'],
 })
-
 export class Tab2Page {
 	public bitcoinValue;
 	public ethereumValue;
@@ -20,23 +19,22 @@ export class Tab2Page {
 	public wibxValue;
 	public xrpValue;
 
-	public bitcoinAVG;
-	public ethereumAVG;
-	public bitcoinCashAVG;
-	public chilizAVG;
-	public chainlinkAVG;
-	public litecoinAVG;
-	public paxGoldAVG;
-	public usdCoinAVG;
-	public wibxAVG;
-	public xrpAVG;
+	public bitcoinData = [];
+	public ethereumData;
+	public bitcoinCashData;
+	public chilizData;
+	public chainlinkData;
+	public litecoinData;
+	public paxGoldData;
+	public usdCoinData;
+	public wibxData;
+	public xrpData;
 
-	public dataAtual;
+	public bitcoinString;
 
 	constructor(private apiService: ApiService) {
 		this.getCoin();
-		this.getData();
-		this.dataHoje();
+		this.getMensal();
 	}
 	getCoin() {
 		this.apiService.getCoin('BTC').subscribe(data => {
@@ -101,51 +99,59 @@ export class Tab2Page {
 			this.xrpValue = valorVenda;
 		});
 	}
-	getData(){
-		this.apiService.getData('XRP').subscribe(data => {
-			console.log(data);
-			var valorMedio = data['avg_price'];
-			valorMedio = parseFloat(valorMedio).toFixed(2);
-			console.log('xrpValue: ', valorMedio);
-			this.xrpAVG = valorMedio;
-		});
-	}
-	dataHoje() {
+
+	getMensal() {
 		var data = new Date();
 		var dia = data.getDate();
-		var mes = data.getMonth() + 1;
-		var ano = data.getFullYear();
-		return this.dataAtual=(dia+"/"+mes+"/"+ano);
+		var diaAnterior = dia - 1;
+
+		for (var i = 1; i <= diaAnterior; i++) {
+			this.apiService.getMensal('BTC', i).subscribe(data => {
+				this.bitcoinData.push(data['avg_price']);
+				this.bitcoinString = JSON.stringify(this.bitcoinData);
+			});
+		}
 	}
 
 	public pieChart: GoogleChartInterface = {
 		chartType: 'LineChart',
 		dataTable: [
-		  ['Task', 'Hours per Day'],
-		  ['Work',     50],
-		  ['Eat',      2],
-		  ['Commute',  33],
-		  ['Watch TV', 5],
-		  ['Sleep',    7]
+			['Task', 'Hours per Day'],
+			['Work', 50],
+			['Eat', 2],
+			['Commute', 33],
+			['Watch TV', 5],
+			['Sleep', 7],
 		],
 		//firstRowIsData: true,
-		options: {'title': 'Tasks'},
-	  };
+		options: {title: 'Tasks'},
+	};
 
-	  public candlestickChart : GoogleChartInterface = {
+	public candlestickChart: GoogleChartInterface = {
 		chartType: 'CandlestickChart',
 		dataTable: [
-		  ['Hora', 'Valor', '1', '2', '3'],
-		  ['10 am', 1 , 20, 1 , 1],
-		  ['11 am', 1, 1, 20, 1],
-		  ['12 am', 1, 1, 1, 20],
-		  ['1 pm', 20, 1, 1, 1],
-		  ['2 pm', 1, 27, 9, 10],
-		  ['3 pm', 1, 37, 9, 10],
-		  ['4 pm', 1, 60, 9, 10],
-		  ['5 pm', 1, 75, 9, 10]
+			['Hora', 'Valor', '1', '2', '3'],
+			['10 am', 1, 20, 1, 1],
+			['11 am', 1, 1, 20, 1],
+			['12 am', 1, 1, 1, 20],
+			['1 pm', 20, 1, 1, 1],
+			['2 pm', 1, 27, 9, 10],
+			['3 pm', 1, 37, 9, 10],
+			['4 pm', 1, 60, 9, 10],
+			['5 pm', 1, 75, 9, 10],
 		],
 		//firstRowIsData: true,
-		options: {'title': 'Tasks'},
-	  };
+		options: {title: 'Tasks'},
+	};
+
+	public graficoBitcoin: GoogleChartInterface = {
+		chartType: 'LineChart',
+		dataTable: [
+			['Data', 'Preço'],
+			[this.bitcoinData[0], parseInt(this.bitcoinData[1])],
+			[this.bitcoinData[2], parseInt(this.bitcoinData[3])],
+		],
+		//firstRowIsData: true,
+		options: {title: 'Tasks'},
+	};
 }
